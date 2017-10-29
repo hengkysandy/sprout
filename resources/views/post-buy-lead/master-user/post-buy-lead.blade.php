9b42901b146f450a9c71d13531614fa235ae5d48
andydaz <andy.daz.95@gmail.com>
2017-10-29 11:18:00 +0700

    fix menu master

--- resources/views/post-buy-lead/master-user/post-buy-lead.blade.php
+++ resources/views/post-buy-lead/master-user/post-buy-lead.blade.php
@@ -715,32 +715,32 @@
       <div class="col-md-3 col-sm-12 col-xs-12 hide-on-med-and-down">
         <ul class="no-ul-style menu-wrapper">
           <li>
-            <a href="home.html" class="btn btn-orange btn-lg padding-transition no-border-radius">
+            <a href="home" class="btn btn-orange btn-lg active-orange padding-transition no-border-radius">
               <i class="pull-left fa fa-home padding-top-2px padding-right-8px"></i> <span>Home</span>
             </a>
           </li>
           <li>
-            <a href="post-buy-lead.html" class="btn btn-orange active-orange btn-lg padding-transition no-border-radius">
-              <i class="pull-left fa fa-pencil-square padding-top-2px padding-right-8px"></i> <span>Buy Lead</span>
+            <a href="post-buy-lead" class="btn btn-orange btn-lg padding-transition no-border-radius">
+              <i class="pull-left fa fa-pencil-square padding-top-2px padding-right-8px"></i> <span>Post Buy Lead</span>
             </a>
           </li>
           <li>
-            <a href="company-database.html" class="btn btn-orange btn-lg padding-transition no-border-radius">
+            <a href="company-database" class="btn btn-orange btn-lg padding-transition no-border-radius">
               <i class="pull-left fa fa-building padding-top-2px padding-right-8px"></i> <span>Company Database</span>
             </a>
           </li>
           <li>
-            <a href="meeting-schedule.html" class="btn btn-orange btn-lg padding-transition no-border-radius">
+            <a href="meeting-schedule" class="btn btn-orange btn-lg padding-transition no-border-radius">
               <i class="pull-left fa fa-calendar padding-top-2px padding-right-8px"></i> <span>Meeting Schedule</span>
             </a>
           </li>
           <li>
-            <a href="profile.html" class="btn btn-orange btn-lg padding-transition no-border-radius">
+            <a href="profile" class="btn btn-orange btn-lg padding-transition no-border-radius">
               <i class="pull-left fa fa-gear padding-top-2px padding-right-8px"></i> <span>Profile</span>
             </a>
           </li>
           <li>
-            <a href="../home-login.html" class="btn btn-orange btn-lg padding-transition no-border-radius">
+            <a href="logoutUser" class="btn btn-orange btn-lg padding-transition no-border-radius">
               <i class="pull-left fa fa-power-off padding-top-2px padding-right-8px"></i> <span>Logout</span>
             </a>
           </li>
